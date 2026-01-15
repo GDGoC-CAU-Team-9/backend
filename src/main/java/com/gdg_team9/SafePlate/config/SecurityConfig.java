ce97bc9b41284897e149dea2825b6a9e00d9e243
noir1458 <noir1458@gmail.com>
2026-01-15 17:23:36 +0900

    feat: implement core security infrastructure and member domain

---
+++ src/main/java/com/gdg_team9/SafePlate/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.gdg_team9.SafePlate.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login","/join").permitAll()
+                .anyRequest().authenticated()
+            )
+                .formLogin(form -> form
+                        .usernameParameter("email")
+                        .defaultSuccessUrl("/")
+                        .permitAll()
+                );
+        return http.build();
+    }
+}
---
+++ src/main/java/com/gdg_team9/SafePlate/domain/Member.java
@@ -0,0 +1,30 @@
+package com.gdg_team9.SafePlate.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String name;
+
+    @Builder
+    public Member(String email,String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}

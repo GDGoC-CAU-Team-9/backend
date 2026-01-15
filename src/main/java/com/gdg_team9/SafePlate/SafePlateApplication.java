202c0d3c62fb102b78d798e8f753c295c6f9dc12
noir1458 <noir1458@gmail.com>
2026-01-15 15:53:07 +0900

    feat: initialize project with spring boot and dependencies

---
+++ src/main/java/com/gdg_team9/SafePlate/SafePlateApplication.java
@@ -0,0 +1,13 @@
+package com.gdg_team9.SafePlate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafePlateApplication.class, args);
+	}
+
+}

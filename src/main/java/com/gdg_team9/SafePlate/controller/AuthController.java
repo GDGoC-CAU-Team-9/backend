6f2a858e932adfac190977b279cb4a5bd7fea2d0
noir1458 <noir1458@gmail.com>
2026-01-15 21:01:46 +0900

    feat: refactor join API with validation and standardized response

--- src/main/java/com/gdg_team9/SafePlate/controller/AuthController.java
+++ src/main/java/com/gdg_team9/SafePlate/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.gdg_team9.SafePlate.controller;
 
 import com.gdg_team9.SafePlate.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +15,8 @@ public class AuthController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public String join(@RequestBody JoinRequest request){
+    public ResponseEntity<String> join(@Valid @RequestBody JoinRequest request){
         memberService.join(request.getEmail(), request.getPassword(), request.getName());
-        return "회원가입 성공";
+        return new ResponseEntity<>("회원가입 성공", HttpStatus.CREATED);
     }
 }
--- src/main/java/com/gdg_team9/SafePlate/controller/JoinRequest.java
+++ src/main/java/com/gdg_team9/SafePlate/controller/JoinRequest.java
@@ -1,5 +1,8 @@
 package com.gdg_team9.SafePlate.controller;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -10,7 +13,14 @@ import lombok.AllArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class JoinRequest {
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
     private String password;
+
+    @NotBlank(message = "이름은 필수 입력 항목입니다.")
     private String name;
 }

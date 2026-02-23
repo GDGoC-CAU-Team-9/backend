65ca1ea914ff25bcbdfdcce98d2a5aa3aad0d7c4
cau20232907 <hs20232907@cau.ac.kr>
2026-02-24 01:23:56 +0900

    refactor: changed team search to avoid all ingredients
    Signed-off-by: cau20232907 <hs20232907@cau.ac.kr>

--- src/main/java/com/gdg_team9/SafePlate/restaurant/service/RestaurantService.java
+++ src/main/java/com/gdg_team9/SafePlate/restaurant/service/RestaurantService.java
@@ -42,9 +42,36 @@ public class RestaurantService {
             throw new GeneralException(ErrorStatus.FILE_NOT_OWNED);
         }
 
+        // TODO 팀 기능 추가에 따라 Set으로의 변경 검토
+        List<String> userAllergies;
+
         Long teamMemberId = clientSearchRequest.getTeamMemberId();
         if (teamMemberId == null) {
-            RestaurantSearchResult searchResult = searchRestaurantForOneMember(member, imageUrls);
+            userAllergies = userAllergyRepository.findByMember(member).stream()
+                    .map(userAllergy -> userAllergy.getAllergy().getName())
+                    .toList();
+        } else {
+            TeamMember teamMember =
+                    teamMemberRepository.findByIdAndMember(teamMemberId, member)
+                            .orElseThrow(() -> new GeneralException(ErrorStatus.TEAM_NOT_FOUND));
+
+            // TODO 기피 재료 저장 방식 변경에 따른 수정 필요
+            // 추후 코드가 완전히 변경됨에 따라, 현재는 N+1 문제가 발생하는 임시 코드로 설정
+            userAllergies = teamMember.getTeam().getTeamMembers().stream()
+                    .flatMap(
+                            tm -> userAllergyRepository.findByMember(tm.getMember()).stream()
+                                    .map(userAllergy -> userAllergy.getAllergy().getName())
+                    )
+                    .toList();
+        }
+
+        // TODO 이미지 여러 개 보낼 수 있도록 수정
+        AiClientRequest.SearchRequest aiSearchRequest = AiClientRequest.SearchRequest.builder()
+                .imageUrl(imageUrls.get(0))
+                .avoid(userAllergies)
+                .build();
+        try {
+            RestaurantSearchResult searchResult = aiClient.requestSearch(aiSearchRequest).getBody();
 
             SearchHistory searchHistory = SearchHistory.builder()
                     .member(member)
@@ -54,53 +81,7 @@ public class RestaurantService {
 
             searchHistoryRepository.save(searchHistory);
             return searchResult;
-        } else {
-            TeamMember teamMember =
-                    teamMemberRepository.findByIdAndMember(teamMemberId, member)
-                            .orElseThrow(() -> new GeneralException(ErrorStatus.TEAM_NOT_FOUND));
 
-            List<SearchHistory> searchResults = teamMember.getTeam().getTeamMembers().parallelStream()
-                    .map(tm -> {
-                        // 팀원 각각에 대한 검색
-                        RestaurantSearchResult searchResult =
-                                searchRestaurantForOneMember(tm.getMember(), imageUrls);
-
-                        // 검색결과를 검색기록 형태로 변경 (검색 결과 + 검색 대상자를 mapping한 것이기도 함)
-                        return SearchHistory.builder()
-                                .member(tm.getMember())
-                                .imageIds(clientSearchRequest.getIds())
-                                .searchResult(searchResult)
-                                .build();
-                    })
-                    .toList();
-
-            searchHistoryRepository.saveAll(searchResults);
-
-            // TODO 팀원 각각의 검색 결과 반환
-
-            // 검색한 사람의 검색 결과 반환
-            for (SearchHistory searchHistory : searchResults) {
-                if (searchHistory.getMember().getId().equals(member.getId())) {
-                    return searchHistory.getSearchResult();
-                }
-            }
-            throw new GeneralException(ErrorStatus._INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    private RestaurantSearchResult searchRestaurantForOneMember(Member member, List<String> imageUrls) {
-        // TODO 팀 검색 시 최적화 필요 (팀원 각각의 알레르기 정보 조회 -> N+1 문제)
-        List<String> userAllergies = userAllergyRepository.findByMember(member)
-                .stream().map(userAllergy -> userAllergy.getAllergy().getName())
-                .toList();
-
-        // TODO 이미지 여러 개 보낼 수 있도록 수정
-        AiClientRequest.SearchRequest aiSearchRequest = AiClientRequest.SearchRequest.builder()
-                .imageUrl(imageUrls.get(0))
-                .avoid(userAllergies)
-                .build();
-        try {
-            return aiClient.requestSearch(aiSearchRequest).getBody();
         } catch (feign.RetryableException e) {
             throw new GeneralException(ErrorStatus.AI_CONNECT_FAIL);
         } catch (feign.FeignException e) {

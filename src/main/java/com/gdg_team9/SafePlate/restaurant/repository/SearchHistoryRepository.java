b098a91d8ac94dc29871017422bc36a99814bd42
noir1458 <noir1458@gmail.com>
2026-03-13 19:55:48 +0900

    fix: add delete search-history service implementation

--- src/main/java/com/gdg_team9/SafePlate/restaurant/repository/SearchHistoryRepository.java
+++ src/main/java/com/gdg_team9/SafePlate/restaurant/repository/SearchHistoryRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long> {
     Page<SearchHistory> findByMember(Member member, Pageable pageable);
+    Optional<SearchHistory> findByIdAndMember(Long id, Member member);
 }
